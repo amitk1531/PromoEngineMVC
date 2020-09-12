89fc6ef3fc6e24e15ace23d7809d02bb3ff8b6e6
amitk1531 <amitk1531@gmail.com>
2020-09-12 15:57:21 +0530

    Added Model

---
+++ PromotionEngine/Models/PromotionEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PromotionEngine.Models
+{
+    public class PromotionEngine
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public int Total { get; set; }
+    }
+}
